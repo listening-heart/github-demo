207bb58e664041cec3d09bf641e296988e1127e6
wind_zz <learning_heart@163.com>
2020-11-25 11:09:27 +0800

    '第六次提交,修改user文件'

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 console.log('gitgub-demo')
 
-
+var user = [{
+    id: 1,
+    user: 'admin',
+    password: 123456
+}, {
+    id: 2,
+    user: 'zhangsan',
+    password: 654321
+}, {
+    id: 3,
+    user: 'xiaoming',
+    password: 111222
+}]
